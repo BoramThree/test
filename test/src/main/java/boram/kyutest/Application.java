41e9f8b679245a90d815d7046dbdf332b81a21eb
C:\Lecture <https://github.com/rlaskarb/240826-.git>
2024-10-04 17:21:00 +0900

    20241004 test

---
+++ test/src/main/java/boram/kyutest/Application.java
@@ -0,0 +1,11 @@
+package boram.kyutest;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("만나서 반갑습니다.");
+
+
+
+    }
+}
